78388da000aa7c533b8c4dcd384af0b4a7e937cd
unknown <tnie2039@scs.ubbcluj.ro>
2018-04-01 23:29:20 +0300

    lll

--- src/test/java/lab1/RepositoryTest.java
+++ src/test/java/lab1/RepositoryTest.java
@@ -31,7 +31,7 @@ public class RepositoryTest {
     }
 
     @Test
-    public void test2() throws IOException {
+    public void test2()  {
         String patients = "FilePatients.txt";
         String consultations = "FileConsultations.txt";
         repository.Repository repo = new repository.Repository(patients, consultations);
@@ -40,7 +40,11 @@ public class RepositoryTest {
         List<String> a  = new ArrayList<String>();
         a.add("sup1");
         model.Consultation c = new Consultation("1","1111111111111","a",a,"02/02/1999");
-        repo.addConsultationToFile(c);
+        try {
+            repo.addConsultationToFile(c);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         assertEquals( l + 1 , repo.getConsultationList());
     }
 }
